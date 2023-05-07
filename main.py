dbda4dfeda06f26972b28bef2e243a86f9bd4492
Tecmac <eraykayur@gmail.com>
2023-05-08 00:12:39 +0200

    optimized and integrated driver fetching

--- main.py
+++ main.py
@@ -2,30 +2,44 @@ import psycopg2
 import urllib.request, json
 
 def fetchCircuits():
-    with urllib.request.urlopen("http://ergast.com/api/f1/circuits.json") as url:
+    with urllib.request.urlopen("http://ergast.com/api/f1/circuits.json?limit=1000") as url:
         data = json.load(url)
         print(data)
+        count1 = 0
+        count2 = 0
+        for circuit in data["MRData"]["CircuitTable"]["Circuits"]:
 
-    for circuit in data["MRData"]["CircuitTable"]["Circuits"]:
-        try:
-            cur.execute("INSERT INTO grandprix (name,land,ort) VALUES (%s, %s, %s)", (circuit["circuitName"], circuit["Location"]["country"], circuit["Location"]["locality"]))
-        except:
-            print("Already up-to-date")
+                try:
+                    cur.execute("INSERT INTO grandprix (name,land,ort) VALUES (%s, %s, %s)",(circuit["circuitName"], circuit["Location"]["country"], circuit["Location"]["locality"]))
+                    print("Die Strecke "+ circuit["circuitName"]+" wurde hinzugefügt :)")
+                    count1 += 1
+                except:
+                    conn.rollback() #need a rollback to retry
+                    count2 += 1
+
+    print("Already up-to-date: ",count2)
+    print("Added: ",count1)
 
 def fetchDrivers():
-    with urllib.request.urlopen("http://ergast.com/api/f1/drivers.json") as url:
+    with urllib.request.urlopen("http://ergast.com/api/f1/2023/drivers.json?limit=1000") as url: #
         data = json.load(url)
         print(data)
-
-    for circuit in data["MRData"]["DriverTable"]["Circuits"]:
+        count1 = 0
+        count2 = 0
+    for drivers in data["MRData"]["DriverTable"]["Drivers"]:
         try:
-            cur.execute("INSERT INTO grandprix (name,land,ort) VALUES (%s, %s, %s)",
-                        (circuit["circuitName"], circuit["Location"]["country"], circuit["Location"]["locality"]))
+            cur.execute("INSERT INTO Fahrer (name,vorname,nationalität,rennnumer,geburtstag) VALUES (%s, %s, %s,%s,%s)",(drivers["givenName"], drivers["familyName"], drivers["nationality"], drivers["permanentNumber"],drivers["dateOfBirth"]))
+            print("Der Fahrer "+ drivers["givenName"]+drivers["familyName"]+" wurde hinzugefügt :)")
+            count1 += 1
         except:
-            print("Already up-to-date")
+            conn.rollback()
+            count2 += 1
+    print("Already up-to-date: ",count2)
+    print("Added: ",count1)
+
 
 try:
-    conn = psycopg2.connect(  #making a connection to the server
+    conn = psycopg2.connect(  # making a connection to the server
         host="localhost",
         database="racing",
         user="postgres",
@@ -36,8 +50,7 @@ except:
 cur = conn.cursor()
 
 fetchCircuits()
-
-
+fetchDrivers()
 
 conn.commit()
 cur.close()
