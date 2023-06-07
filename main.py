a69edc3eeb69716bf84c54d6bd31156e9354152c
Tecmac <eraykayur@gmail.com>
2023-06-07 13:13:51 +0200

    implemented ui

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import psycopg2
 import urllib.request, json
 from datetime import date
 
+from Player import Player
 
-def createPlayer(gamertag,name,forename,password):
-    cur.execute("INSERT INTO player(GAMERTAG, NAME, FORENAME, PASSWORD) VALUES (%s,%s,%s,%s)",(gamertag,name,forename,password))
 
-def einloggen(gamertag,password):
+def createPlayer(gamertag, name, forename, password):
+    player = Player(gamertag, name, forename, password)
+    player.save()
+    #cur.execute("INSERT INTO player(GAMERTAG, NAME, FORENAME, PASSWORD) VALUES (%s,%s,%s,%s)",(gamertag,name,forename,password))
+
+def einloggen(gamertag, password):
     cur.execute("Select gamertag From player where password = %s and gamertag= %s ",(gamertag,password))
 
     if cur.fetchone() is not None:
@@ -26,22 +30,9 @@ except:
 
 cur = conn.cursor()
 
-with urllib.request.urlopen("http://ergast.com/api/f1/"+ str(date.today().year)+
-                            "/drivers.json?limit=1000") as url:
-    test = json.load(url)
-    print(test)
-    print(date.today())
+createPlayer("d", "d", "d", "e")
 
-    for data in test["MRData"]["DriverTable"]["Drivers"]:
-        cur.execute(
-                "SELECT name, forename from driver where name = %s and forename = %s and nationality =%s and racenumber=%s and birthday =%s",
-                (data["familyName"], data["givenName"], data["nationality"],
-                 data["permanentNumber"], data["dateOfBirth"]))
-        print(cur.fetchall())
-    cur.execute(
-        "SELECT race.name, circuit.name from race inner join circuit on race.circuit_id = circuit.circuit_id where date> %s order by date",
-        (date.today(),))
-
+#
 
     #fahrer auswählen
 
